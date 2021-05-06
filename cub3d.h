2792b7eb4834a99c3d08f1fa7a9436b57d52ef5a
Lidia Krinova <lkrinova@gmail.com>
2021-05-06 19:12:13 +0300

    Update cub3d.h

--- cub3d.h
+++ cub3d.h
@@ -20,7 +20,7 @@
 # include <stdio.h>
 # include "libft/libft.h"
 # include "minilibx_opengl/mlx.h"
-# define MAP_SCALE 16
+# define M_SCALE 16
 # define WKEY 13
 # define SKEY 1
 # define AKEY 0
@@ -32,14 +32,14 @@
 
 typedef struct s_fxy
 {
-	float 		x;
-	float 		y;
+	double 		x;
+	double 		y;
 }				t_fxy;
 
 typedef struct s_xy
 {
-	float 		x;
-	float 		y;
+	double 		x;
+	double 		y;
 }				t_xy;
 
 typedef struct  s_img {
@@ -50,41 +50,40 @@ typedef struct  s_img {
 }               t_img;
 
 typedef struct  s_vars {
-	void        *mlx_ptr;
-	void        *win_ptr;
+	void        *ml_p;
+	void        *w_p;
 	t_img 		img;
 }               t_vars;
 
 typedef struct s_player
 {
 	t_fxy 		pos;
-	float 		pov;
+	double 		pov;
 	t_fxy		ray;
-	float		ray_st;
-	float 		ray_end;
+    double		r_st;
+    double 		r_end;
 
 }				t_player;
 
 typedef struct s_rays
 {
-	float 		dist;
-	t_xy 		ray_pos;
-	float 		size;
-	int 		hitx;
-	int 		hity;
-	int 		hit_text;
-
+    double 		dist;
+	t_xy 		r_pos;
+    double 		size;
+	double 		hitx;
+	double 		hity;
+	int         qdrnt;
 }				t_rays;
 
 typedef struct s_textur
 {
-	char 		*image;
-	int			width;
-	int			height;
+	char 		*ig;
+	int	    	wid;
+	int 		hei;
 	char 		*addr;
 	int			bpp;
-	int			size_line;
-	int			endian;
+	int			sz_l;
+	int			end;
 }				t_textur;
 
 typedef struct 	s_map
@@ -92,7 +91,7 @@ typedef struct 	s_map
 	t_xy 		size;
 	char		**map_ar;
 	int 		m_start;
-	t_player	player;
+	t_player	plr;
 	t_textur 	we;
 	t_textur 	no;
 	t_textur 	so;
@@ -112,9 +111,9 @@ typedef struct s_cub
 	char 		*sprite;
 	int 		floor_rgb;
 	int			ceil_col;
-	float 		dist_plane;
-	t_map 		allmap;
-	t_rays 		*rays;
+	double 		dist_plane;
+	t_map 		m;
+	t_rays 		*rs;
 	t_vars 		ml;
 }				t_cub;
 
@@ -131,19 +130,21 @@ void	get_r_rgb(char *s, t_cub *flags);
 int		check_rgb(char *s);
 int 	is_valid(char **ar);
 void 	draw_sqaure(int x, int y, t_img img, int color);
-void    pixel_put(t_img *img, int x, int y, int color);
+void    pixel_put(t_img *img, int x, int y, unsigned int color);
 unsigned int pixel_get(t_textur *textura, int x, int y);
 void 	draw_map(t_cub flags);
 void 	draw_player(int x, int y, t_img img, int color);
-void	draw_col(t_cub *fl, int text, int col_num);
+void	draw_col(t_cub *fl, t_textur *tx, int c_n);
 int		move(int keycode, t_cub *fl);
 void	clean_s(t_cub flags);
-int		render(t_cub *flags);
+int		render(t_cub *fl);
 float	get_pov(char c);
 void	get_texture(char *str, t_cub *flagg);
 void draw_texture(t_cub *fl);
 void	get_text(t_cub *fl);
 void 	cast_view(t_player *pl, t_cub *fl);
 void	rays_arr_init(t_cub *flags);
+int     f_quadrant(double angle);
+double is_wall(t_cub *fl, double x, double y);
 
 #endif
